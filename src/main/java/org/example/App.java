2f957db47a9c6018f560ec74c0d4d7ff5bf8cf45
Flavio Reis <ext.flavios@unicid.edu.br>
2023-03-28 19:36:39 -0300

    Exibindo e manipulando dados

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,20 +10,39 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         // Fazer uma conexão HTTP e buscar os tops 250 filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
         URI adress = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(adress).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
         // Extrair só os dados que interessam (título, poster, classificação)
         var parser = new JsonParser();
         List<Map<String, String>> movieList = parser.parse(body);
 
-        // exibir e manipular dados
 
+        // exibir e manipular dados
+        for (Map<String, String> movie : movieList) {
+            System.out.println("\u001b[36m" + movie.get("title") + "\u001b[0m");
+            System.out.println(movie.get("image"));
+            System.out.println("\u001b[30m \u001b[41m Classificação " + movie.get("imDbRating") + "\u001b[0m");
+            printRating(movie.get("imDbRating"));
+            System.out.println();
+        }
     }
+    public static void printRating(String rating) {
+        int nota = (int) Math.round(Double.parseDouble(rating));
+        String cor = "\u001b[33m"; // código ANSI para a cor amarela
+        String reset = "\u001b[0m"; // código ANSI para resetar a cor
+
+        for (int i = 0; i < nota; i++) {
+            System.out.print(cor + "\uD83C\uDF1F" + reset);
+        }
+        System.out.println();
+    }
+
+
 }
